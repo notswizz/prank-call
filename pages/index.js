4af1188d9f42770cf5993dd56ab0a384217b9f90
swizz <emailswizz@gmail.com>
2024-05-21 01:16:07 -0400

    ok

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center text-white relative bg-gradient-to-r from-gray-900 via-gray-700 to-gray-900">
       <button
-        onClick={() => signIn('google', { callbackUrl: '/prank' })}
+        onClick={() => signIn('google', { callbackUrl: `${window.location.origin}/prank` })}
         className="flex items-center bg-blue-600 text-white px-6 py-3 rounded-full shadow-lg hover:bg-blue-700 transition duration-300 absolute top-4 right-4"
       >
         <Image src="/google.png" alt="Google Logo" width={24} height={24} className="mr-3" />
